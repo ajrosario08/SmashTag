758f774ebb9fc877af0eb5e672611115a3a0b340
Anthony Rosario <ajrosario08@gmail.com>
2015-12-06 20:03:15 -0800

    imported twitter swift files

---
+++ TweetTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TweetTableViewController.swift
+//  SmashTag
+//
+//  Created by Anthony Rosario on 11/15/15.
+//  Copyright © 2015 Anthony Rosario. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewController: UITableViewController {
+
+    var tweets = [[Tweet]]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
