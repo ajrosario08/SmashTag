44b6d5bafc2da3a6294854c1070d5e70d7e6889c
Anthony Rosario <ajrosario08@gmail.com>
2015-12-06 20:35:57 -0800

    populated table view cells with tweets

--- TweetTableViewController.swift
+++ TweetTableViewController.swift
@@ -14,6 +14,39 @@ class TweetTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let request = TwitterRequest(search: "#stanford", count: 100)
+        request.fetchTweets { (newTweets) -> Void in
+            dispatch_async(dispatch_get_main_queue()) { () -> Void in
+                if newTweets.count > 0 {
+                    self.tweets.insert(newTweets, atIndex: 0)
+                    self.tableView.reloadData()
+                }
+            }
+        }
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return tweets.count
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tweets[section].count
+    }
+    
+    private struct Storyboard {
+        static let CellReuseIdentifier = "Tweet"
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(Storyboard.CellReuseIdentifier, forIndexPath: indexPath) as UITableViewCell
+        
+        
+        let tweet = tweets[indexPath.section][indexPath.row]
+        cell.textLabel?.text = tweet.text
+        cell.detailTextLabel?.text = tweet.user.name
+        
+        return cell
     }
     
 }
